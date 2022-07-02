1b103c0f63348873a2174549bfb7cbe281008822
nagini s <nagini18@gmail.com>
2022-07-01 23:11:22 -0400

    Starting my python Journey from scratch :) Here is my learnings on introduction and basic data types

---
+++ DrinkTheBasic/LetsStartFromSratch.py
@@ -0,0 +1,11 @@
+
+# Variables and its data structures
+#python have 4 types of built in Data Structures namely List, Dictionary, Tuple and Set.
+myNumber = 3
+print(myNumber)
+
+myNumber = 4.5
+print(myNumber)
+
+myNumber = "Hello Beautiful!!!"
+print(myNumber)
